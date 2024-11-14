605314679088ecf6a5350e5bb4e738409aa9c1ff
Kashvi Kapoor <kashvi.kapoor@student.vinci.be>
2024-11-14 18:04:32 +0100

    fix : tests step 1 (converted to digital time)

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -5,28 +5,28 @@ describe("Berlin clock kata project", function() {
     
     // --------------------  STEP 1 : Simple Minute  --------------------
 
-    it("BerlinClock should return 0000 when given 0", function() {
-        const result = main.getSimpleMinute(0);
+    it("BerlinClock should return 0000 when given 00:00:00", function() {
+        const result = main.getSimpleMinute("00:00:00");
         expect(result).toBe("0000");
     });
 
-    it("BerlinClock should return Y000 when given 1", function() {
-        const result = main.getSimpleMinute(1);
+    it("BerlinClock should return Y000 when given 00:01:00", function() {
+        const result = main.getSimpleMinute("00:01:00");
         expect(result).toBe("Y000");
     });
 
-    it("BerlinClock should return YY00 when given 2", function() {
-        const result = main.getSimpleMinute(2);
+    it("BerlinClock should return YY00 when given 00:02:00", function() {
+        const result = main.getSimpleMinute("00:02:00");
         expect(result).toBe("YY00");
     });
 
-    it("BerlinClock should return YYY0 when given 3", function() {
-        const result = main.getSimpleMinute(3);
+    it("BerlinClock should return YYY0 when given 00:03:00", function() {
+        const result = main.getSimpleMinute("00:03:00");
         expect(result).toBe("YYY0");
     });
 
-    it("BerlinClock should return YYYY when given 4", function() {
-        const result = main.getSimpleMinute(4);
+    it("BerlinClock should return YYYY when given 00:04:00", function() {
+        const result = main.getSimpleMinute("00:04:00");
         expect(result).toBe("YYYY");
     });
 
