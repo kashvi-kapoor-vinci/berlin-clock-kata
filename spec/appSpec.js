b343be08f6129a9f0eb3e283bfc957566b269da3
Arwa Sheikh Idries <arwa.sheikh@student.vinci.be>
2024-11-14 12:54:51 +0100

    add : const (combined tests)

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -133,10 +133,12 @@ describe("Berlin clock kata project", function() {
     // --------------------  STEP 1, 2, 3 and 4 : Combined tests  --------------------
 
     it("BerlinClock should return RRR0:RR00:00000000000:0000 when given 17:00:00", function() {
+        const result = main.berlinClock(17,0);
         expect(result).toBe("RRR0\nRR00\n00000000000\n0000");
     });
 
     it("BerlinClock should return RRRR:R000:YYRYYRYYRYY:YYYY when given 21:59:00", function() {
+        const result = main.berlinClock(21,59);
         expect(result).toBe("RRRR\nR000\nYYRYYRYYRYY\nYYYY");
     });
 
