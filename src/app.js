d859c1837b06d0b3349b737aa158e5f229279ef1
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2024-11-14 11:29:40 +0100

    Implement : method to return simple minutes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
 export class Main {
 
+    
+
+    //Returns the display of simple minutes as a string.
+    getSimpleMinute(time){
+        const minutes = this.getMinutes(time);
+        const simpleMinutes = minutes % 5;
+        return "Y".repeat(simpleMinutes) + "0".repeat(Math.abs(4-simpleMinutes));
+    }
 }
