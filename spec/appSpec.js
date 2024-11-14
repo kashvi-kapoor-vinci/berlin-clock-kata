59203506a01ce229cce500672f850594e226e7f2
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2024-11-14 11:15:58 +0100

    add : test 0min for step 1

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,1 +1,11 @@
 import {Main} from '../src/app.js';
+
+describe("Berlin clock kata project", function() {
+    let main = new Main();
+    
+    // --------------------  STEP 1 : Single Minute  --------------------
+
+    it("BerlinClock should return 0000 when given 0", function() {
+        expect(0).toBe("0000");
+    });
+})
