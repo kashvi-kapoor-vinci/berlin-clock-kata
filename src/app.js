3125b6eb4e00b4fd788d55945e06bf752c9b0eeb
Arwa Sheikh Idries <arwa.sheikh@student.vinci.be>
2024-11-14 11:47:26 +0100

    implement : method to return five minutes block

--- src/app.js
+++ src/app.js
@@ -12,4 +12,24 @@ export class Main {
         const simpleMinutes = minutes % 5;
         return "Y".repeat(simpleMinutes) + "0".repeat(Math.abs(4-simpleMinutes));
     }
+
+    //Returns the display of five minutes block as a string.
+    getFiveMinutes(minutes) {
+        const lightOn = Math.floor(minutes / 5); 
+        let totalLights = ""; 
+
+        for (let i = 0; i < lightOn; i++) {
+            if ((i + 1) % 3 === 0) {  // multiple of 3 = red light
+                totalLights += "R";
+            } else {
+                totalLights += "Y"; 
+            }
+        }
+
+        while (totalLights.length < 11) {
+            totalLights += "0";
+        }
+
+        return totalLights; 
+    }
 }
